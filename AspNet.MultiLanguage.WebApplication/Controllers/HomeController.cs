a56cd9be8900b1876616f8c10ff68bd1ad32c0f8
Elmer <elmer@cangahuala.dev>
2021-03-14 18:08:29 -0500

    Uploading default project template

---
+++ AspNet.MultiLanguage.WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using AspNet.MultiLanguage.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNet.MultiLanguage.WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
